102e900ee4a4a0fdc6953f49aec68bbd84a600a3
Nathan Cyr <nathan.cyr@gmail.com>
2018-03-22 11:41:46 -0400

    Troisième test

--- TP1_Cyr_Schreyer/src/Partie_3/MainTest.java
+++ TP1_Cyr_Schreyer/src/Partie_3/MainTest.java
@@ -10,25 +10,24 @@ import org.junit.Test;
 public class MainTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
-	
-	@Test
 	public void dateTest() {
 		GregorianCalendar gc = new GregorianCalendar();
 		SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy-HH-mm");
 		String dateStringTest = dateFormat.format(gc.getTime());
 		dateStringTest.equals(Main.date());
-		
+
 	}
-	
+
 	@Test
 	public void tpsTest() {
 		double tps = 1.05;
 		assertEquals(tps, Main.TPS, 0);
 	}
 
-	
+	@Test
+	public void tvqTest() {
+		double tvq = 1.10;
+		assertEquals(tvq, Main.TVQ, 0);
+	}
 
 }
